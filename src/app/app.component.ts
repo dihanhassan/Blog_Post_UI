0ba831e083c9bb5ed501afce464e8f3e6380a445
shihab <atahar.islam@technonext.com>
2024-11-19 18:25:06 +0600

    feat : add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgxSpinnerModule } from 'ngx-spinner';
-
+import { HeaderComponent } from './shared/components/header/header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NgxSpinnerModule],
+  imports: [RouterOutlet,NgxSpinnerModule,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
